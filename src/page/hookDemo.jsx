e9bc4004ed0c106f0d1178d69f8dd767001cd1ac
LinHuaZhen <874035968@qq.com>
2020-06-15 15:33:31 +0800

    [hook]

--- src/page/hookDemo.jsx
+++ src/page/hookDemo.jsx
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 
 function Demo(){
     const [count ,setCount] = useState(0);
+    useEffect(() => {
+        // Update the document title using the browser API
+        console.log(`You clicked ${count} times`)
+      });
     return (
         <div>
+            <p>You clicked {count} times</p>
             <button onClick={()=>setCount(count+1)}>
-                click me
+                click
             </button>
 
         </div>
